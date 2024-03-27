9a90808b639fc62eefe4fa184253a6d47c8a10b5
Denis Revunov <rnovds@gmail.com>
2024-03-27 17:53:59 +0300

    Handle SIGINT/SIGTERM instead of stdin exit command
    Make the first step to running the bot as a service.

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
 	toml "github.com/BurntSushi/toml"
 )
@@ -27,13 +28,14 @@ func main() {
 	}
 
 	go cp.Start()
-	log.Printf("Bot Started, enter q to stop\n")
-	var input string
-	for {
-		fmt.Scanf("%s", &input)
-		if input == "exit" || input == "q" {
-			cp.Stop()
-			return
-		}
+	log.Printf("Bot Started, send ^C to stop\n")
+
+	signalChan := make(chan os.Signal, 2)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
+	for s := range signalChan {
+		log.Println("Received", s.String())
+		cp.Stop()
+		return
 	}
 }
